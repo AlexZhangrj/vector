a00d84a2121081be547bfccde83c2e4882e89108
wangqinghai <212032326@qq.com>
2016-06-13 19:19:12 +0800

    no message

--- testMemory/testMemory/ViewController.swift
+++ testMemory/testMemory/ViewController.swift
@@ -8,12 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+typealias GCDTimerTask = (currentTimer: GCDTimer) -> Void
 
+
+class GCDTimer: NSObject {
+    let _timer: dispatch_source_t
+    private(set) var started: Bool = false
+    let taskClosure: GCDTimerTask
+    
+    
+    init(withTimeInterval time: NSTimeInterval, task: GCDTimerTask, queue: dispatch_queue_t =  dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), delta: Int64 = 2000) {
+        taskClosure = task
+        _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue)
+        dispatch_source_set_timer(_timer, dispatch_walltime(nil, delta), UInt64(Double(NSEC_PER_SEC) * time), 0)
+        super.init()
+        let task: dispatch_block_t = { [weak self] in
+            if let strongSelf = self {
+                strongSelf._doTimer()
+            }
+        }
+        dispatch_source_set_event_handler(_timer, task)
+    }
+    
+    private func _doTimer() {
+        taskClosure(currentTimer: self)
+    }
+    
+    func start() {
+        if !started {
+            dispatch_resume(_timer);
+            started = true
+        }
+    }
+    
+    func stop() {
+        if started {
+            dispatch_source_cancel(_timer)
+            started = false
+        }
+    }
+    
+}
+
+
+
+class ViewController: UIViewController {
+    var timer: GCDTimer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var times: Int = 0
         
+        timer = GCDTimer(withTimeInterval: 0.3, task: { (currentTimer) in
+            print("123")
+            times += 1
+            
+            if times >= 5 {
+                currentTimer.stop()
+                currentTimer.start()
+                times = 0
+            }
+            
+        })
+        
+        timer!.start()
         
         let ttt = MemoryTest()
         let aa = ttt.availableMemory()
